58b2f19db3b7a8de833bfbcba0760f2824120df2
adarapata <efb.youdie@gmail.com>
2012-11-18 23:58:46 +0900

    キャラクターのパラメータクラスとりあえず書いた。
    こっからクラス分けしていく

---
+++ playableCharactar/parameter/CharacterParameter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class CharacterParameter
+{
+	float power;
+	float deffence;
+	float staminaGauge;
+	float staminaRecoverSpeed;
+	float chargeGauge;
+	float chargeSpeed;
+	float weight;
+}
