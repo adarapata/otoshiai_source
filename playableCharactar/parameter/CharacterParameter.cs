4530fa32711b74f20f7a777c8b64c29cf9109a69
adarapata <efb.youdie@gmail.com>
2012-11-19 00:38:59 +0900

    すべてのプリミティブ型をラップした

--- playableCharactar/parameter/CharacterParameter.cs
+++ playableCharactar/parameter/CharacterParameter.cs
@@ -3,11 +3,36 @@ using System.Collections;
 
 public class CharacterParameter
 {
+	private Power power;
+	private Deffence deffence;
+	private Weight weight;
+	private Stamina stamina;
+	private Charge charge;
+}
+
+public class Stamina
+{
+	float quantity;
+	float recoveryRate;
+}
+
+public class Charge
+{
+	float quantity;
+	float speed;
+}
+
+public class Weight
+{
+	float quantity;
+}
+
+public class Deffence
+{
+	float quantity;
+}
+
+public class Power
+{
 	float power;
-	float deffence;
-	float staminaGauge;
-	float staminaRecoverSpeed;
-	float chargeGauge;
-	float chargeSpeed;
-	float weight;
 }
