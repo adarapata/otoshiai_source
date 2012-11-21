108690c5887f03f4c41ad3a4c0e6762ff5fb6d8d
adarapata <efb.youdie@gmail.com>
2012-11-22 02:02:08 +0900

    parameterとcharacterを分けて持って若干見やすく

--- playableCharactar/state/PlayableCharaBaseState.cs
+++ playableCharactar/state/PlayableCharaBaseState.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class PlayableCharaBaseState : BaseState
 {
-	protected PlayableCharacter character;
-	protected CharacterParameter parameter;
+	protected PlayableCharacter character
+	{
+		get;
+		set;
+	}
+	protected CharacterParameter parameter
+	{
+		get { return character.parameter; }
+		set { character.parameter = value; }
+	}
 	public PlayableCharaBaseState(PlayableCharacter parent):base(parent)
 	{
 		character = parent;
-		parameter = parent.parameter;
 	}
 }
 
