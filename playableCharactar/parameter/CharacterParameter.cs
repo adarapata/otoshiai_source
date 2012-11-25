37ca0bd8aef7a80a7324d9908940fa1027492886
adarapata <efb.youdie@gmail.com>
2012-11-25 23:42:24 +0900

    フレームカウント用クラス作った
    名前が微妙

--- playableCharactar/parameter/CharacterParameter.cs
+++ playableCharactar/parameter/CharacterParameter.cs
@@ -59,6 +59,7 @@ public class Stamina
 	{
 		Recovery(recoveryRate);
 	}
+	
 }
 
 public class Charge
@@ -110,5 +111,32 @@ public class Power
 	{
 		get;
 		set;
+	}
+}
+
+public class FrameCounter
+{
+	public int count
+	{
+		get;
+		private set;
+	}
+	public bool IsCall
+	{
+		get
+		{
+		   return count % callTiming == 0;
+		}
+	}
+	private int callTiming;
+	
+	public FrameCounter(int interval)
+	{
+		callTiming = interval;
+	}
+	
+	public void Update()
+	{
+		count++;
 	}
 }
