7550b35e106b697784fdb00005e85c64b7077636
adarapata <efb.youdie@gmail.com>
2013-01-14 18:13:20 +0900

    チームカラークラス作成

--- window/CharacterParameterWindow.cs
+++ window/CharacterParameterWindow.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// チーム色の定数
+/// </summary>
+public class TeamColor
+{
+    public const string RED = "red";
+    public const string BLUE = "blue";
+    public const string YELLOW = "yellow";
+    public const string GREEN = "green";
+    public const string NONE = "none";
+    static private string[] layer_names;
+
+    /// <summary>
+    /// レイヤー番号からチームカラーのSpriteNameを取得
+    /// </summary>
+    /// <param name="layer"></param>
+    /// <returns></returns>
+    static public string GetLayerName(int layer)
+    {
+        if (layer_names == null) { layer_names = new string[] { RED, BLUE, YELLOW, GREEN, NONE }; }
+        return layer_names[layer];
+    }
+}
+
 public class CharacterParameterWindow : MonoBehaviour {
 
     public UISprite teamColor, icon;
@@ -14,22 +38,24 @@ public class CharacterParameterWindow : MonoBehaviour {
     
 	// Use this for initialization
 	void Start () {
-        
-        
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (character == null) Destroy(gameObject);
 	}
 
     /// <summary>
     /// 初期設定
     /// </summary>
-    private void Init()
+    public void Init(Character chara, int number)
     {
-        //オブジェクト名からキャラクターのアイコンを設定
-        icon.spriteName = character.name;
+        character = chara;
+        //オブジェクトのタグからキャラクターのアイコンを設定
+        icon.spriteName = character.tag;
 
+        //キャラのレイヤー番号からチームカラー名を取得
+        teamColor.spriteName = TeamColor.GetLayerName(character.gameObject.layer);
     }
 }
