4435b0cd05164257925836664a021663ff837871
adarapata <efb.youdie@gmail.com>
2013-01-20 23:38:07 +0900

    無敵と点滅処理書いた

--- playableCharactar/parameter/CharacterParameter.cs
+++ playableCharactar/parameter/CharacterParameter.cs
@@ -55,4 +55,59 @@ public class CharacterParameter
         get;
         set;
     }
+
+    /// <summary>
+    /// 無敵状態パラメータ
+    /// </summary>
+    public Invincibly invincibly
+    {
+        get;
+        set;
+    }
+
+    public void Update()
+    {
+        if (invincibly.flag) invincibly.Update();
+    }
+}
+
+/// <summary>
+/// 無敵判定を管理するクラス
+/// </summary>
+public class Invincibly
+{
+    /// <summary>
+    /// ダメージを受けない状態かどうか
+    /// </summary>
+    public bool flag
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// 無敵状態の残時間(フレーム数)
+    /// </summary>
+    public int time
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// 無敵時間の設定
+    /// </summary>
+    /// <param name="setTime">無敵フレーム数</param>
+    /// <param name="isOverride">すでに無敵状態の時に、時間を上書きするか</param>
+    public void Start(int setTime, bool isOverride)
+    {
+        if (flag && !isOverride) return;
+        flag = true;
+        time = setTime;
+    }
+
+    public void Update()
+    {
+        flag = time-- > 0;
+    }
 }
