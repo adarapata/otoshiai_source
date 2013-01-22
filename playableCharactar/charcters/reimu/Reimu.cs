4687a7c55d75abc0cb93dc74b6c0dc1ae061314d
adarapata <efb.youdie@gmail.com>
2013-01-22 21:57:12 +0900

    霊夢の詳細を書いた。御札攻撃の状態を追加した

--- playableCharactar/charcters/reimu/Reimu.cs
+++ playableCharactar/charcters/reimu/Reimu.cs
@@ -1,15 +1,31 @@
 using UnityEngine;
 using System.Collections;
 
-public class Reimu : MonoBehaviour {
+public class Reimu : Character {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    protected override IState CreateSkillState() 
+    {
+        return new ReimuSkillState(this); 
+    }
+
+    void Awake()
+    {
+        InitCharacterParameter(Weight.MIDDLE, 0.2F, 1F, 1F, 0.5F, 3F);
+    }
+    // Use this for initialization
+    void Start()
+    {
+        InitData();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScriptUpdate();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (IsCheckSameTeam(other)) return;
+    }
 }
--- playableCharactar/charcters/reimu/ReimuSkillState.cs
+++ playableCharactar/charcters/reimu/ReimuSkillState.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
 using System.Collections;
 
-public class ReimuSkillState : MonoBehaviour {
+public class ReimuSkillState : CharacterSkillState {
 
-	// Use this for initialization
-	void Start () {
-	
+    private Ofuda ofuda;
+    public ReimuSkillState(Character parent):base(parent)
+	{
+        framecounter = new FrameCounter(10);
+        CreateBullet();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public override System.Type Update()
+	{
+
+        framecounter.Update();
+
+        return framecounter.IsCall ? typeof(CharacterStayState) : null;
 	}
+
+    private void CreateBullet()
+    {
+        var list = AttackLibrary.GetInstance;
+        ofuda = (GameObject.Instantiate(list.ofuda) as GameObject).GetComponent<Ofuda>();
+        ofuda.SetTeamTransform(stateParent.baseParameter.team, stateParent.transform.parent);
+        ofuda.attackParameter.damage = new Damage(30, false, 10, character.frontDirection, false);
+        ofuda.transform.localPosition = stateParent.transform.localPosition;
+        ofuda.baseParameter.moveParameter = new MoveParameter(character.frontDirection, 5F);
+    }
 }
