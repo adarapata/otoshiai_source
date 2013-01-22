28fdf4adee9fd5553adde19f1cfe3aa11b18e008
adarapata <efb.youdie@gmail.com>
2013-01-22 21:55:48 +0900

    攻撃オブジェクトを格納するクラス追加

---
+++ attack/AttackLibrary.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class AttackLibrary : MonoBehaviour {
+    private static AttackLibrary activeLibrary;
+    public GameObject
+        ofuda,
+        amuret;
+	// Use this for initialization
+	void Start () {
+        activeLibrary = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public static AttackLibrary GetInstance
+    {
+        get { return activeLibrary; }
+    }
+}
