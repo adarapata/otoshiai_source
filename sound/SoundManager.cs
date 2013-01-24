c1105b96536df626aad2707d9f63bfa2ea10b1c3
adarapata <efb.youdie@gmail.com>
2013-01-25 00:39:51 +0900

    音声追加

--- sound/SoundManager.cs
+++ sound/SoundManager.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class SoundManager :MonoBehaviour {
 
-    static public AudioSet shot1, amulet;
+    static public AudioSet[] map = new AudioSet[3];
+    static public AudioSet shot1, amulet, fall;
 
     void Start()
     {
+        for (int i = 0; i < 3; i++)
+        {
+            map[i] = new AudioSet("map" + i.ToString(), gameObject);
+        }
+
         shot1 = new AudioSet("shot1", gameObject);
         amulet = new AudioSet("amulet", gameObject);
+        fall = new AudioSet("fall", gameObject);
     }
 
     public static void Play(AudioSet audio)
