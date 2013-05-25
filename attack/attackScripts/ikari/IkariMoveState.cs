cee8983fbf1ad9936a1d1fae844b03a853896b67
adarapata <efb.youdie@gmail.com>
2013-05-25 10:43:34 +0900

    錨オブジェクトとその状態クラスをディレクトリに纏めた

---
+++ attack/attackScripts/ikari/IkariMoveState.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class IkariMoveState : BaseState{
+    protected MoveParameter moveParameter;
+
+    private float accel;
+    private Ikari ikari;
+
+    public override int name { get { return (int)Ikari.STATENAME.Move; } }
+
+	public IkariMoveState(Ikari parent):base(parent)
+	{
+        ikari = parent;
+		moveParameter = stateParent.baseParameter.moveParameter;
+        accel = 0;
+	}
+	
+	public override int Update()
+	{
+		stateParent.transform.localPosition += moveParameter.velocity;
+        moveParameter.speed = accel;
+        accel += 0.1F;
+
+        if (ikari.IsOutMap) { ikari.ChangeNextState(Ikari.STATENAME.Return); }
+
+		return (int)Ikari.STATENAME.Changeless;
+	}
+}
